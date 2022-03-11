2f34fbb6c8a7bc5dee7705e5749f48a11fb7791d
wangjie <2460665525@qq.com>
2022-03-11 22:32:32 +0800

    学习yield内容

--- pytest_source_code/yield_test.py
+++ pytest_source_code/yield_test.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 def yield_test(n):
+    print("第一次返回")
     for i in range(n):
+        # yield 返回call()的值,
+        print(f"{i}的平方", i*i)
+        # 循环体内,或者说迭代器内是每次都执行这个,而不再执行迭代外的内容;
         yield call(i)
         print("i=", i)
     # do something
@@ -12,4 +16,5 @@ def call(i):
 
 # 使用for循环
 for i in yield_test(5):
+    # 将循环的内容打印出来
     print(i, ",")
